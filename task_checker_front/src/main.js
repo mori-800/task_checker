c2e5c95734bd91a00dd51c8b5644e7c4a3a74dfc
AzureAD\moritaizo <mori.taizo@blueship.co.jp>
2025-04-16 11:02:30 +0900

    タスクデータの状態管理を実装

--- task_checker_front/src/main.js
+++ task_checker_front/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import { Modal } from '@kouts/vue-modal'
 import '@kouts/vue-modal/dist/vue-modal.css'
@@ -6,4 +7,5 @@ import './assets/main.css'
 
 const app = createApp(App)
 app.component('Modal', Modal);
+app.use(createPinia())
 app.mount('#app')
