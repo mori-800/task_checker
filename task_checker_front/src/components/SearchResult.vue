74d78f25eefaed22af91c1adfc488d30d457e49e
AzureAD\moritaizo <mori.taizo@blueship.co.jp>
2025-04-17 16:51:48 +0900

    検索結果ページの作成

---
+++ task_checker_front/src/components/SearchResult.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './Header.vue'
+
+</script>
+
+<template>
+  <div class="main">
+    <Header />
+    <div class="contents">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
