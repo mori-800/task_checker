9c9f8a20beee5d8bc7b46e9c024084281bde992c
AzureAD\moritaizo <mori.taizo@blueship.co.jp>
2025-04-17 13:28:17 +0900

    保存データの表示処理を実装

--- task_checker_front/src/stores/TaskStore.js
+++ task_checker_front/src/stores/TaskStore.js
@@ -48,6 +48,7 @@ export const useTaskStore = defineStore('task', () => {
         }
       });
       const addedTask = response.data;
+      tasks.value.push(addedTask)
     }catch(error){
       console.log('タスクデータの保存ができませんでした', error);
     }
